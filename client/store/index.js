6d7418159872cc88ef7fc24e8b60f33c51f35f3f
imparang <jeongjaeyn@gmail.com>
2022-01-19 21:13:20 +0900

    feat: Create configure store with reducer and redux-saga

---
+++ client/store/index.js
@@ -0,0 +1,26 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { createWrapper } from "next-redux-wrapper";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+
+import reducer from "../reducers/index";
+import rootSaga from "../sagas";
+
+const createStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const store = configureStore({
+    reducer,
+    middleware: [sagaMiddleware, logger],
+    devTools: process.env.NODE_ENV !== "production",
+  });
+
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+
+  return store;
+};
+
+const wrapper = createWrapper(createStore, {
+  debug: process.env.NODE_ENV === "development",
+});
+
+export default wrapper;
