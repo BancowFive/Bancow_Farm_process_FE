81018cea69a0392a359e7716b7557f4a2e3ff9de
imparang <jeongjaeyn@gmail.com>
2022-01-23 01:24:13 +0900

    feat: Apply styled button

---
+++ client/components/Button/index.js
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import { StyledButton } from "./style";
+import PropTypes from "prop-types";
+
+const Button = ({
+  variant,
+  children,
+  className,
+  disabled,
+  size = 60,
+  type = "button",
+  onClick,
+  onBlur,
+  onFocus,
+  to,
+  width,
+}) => {
+  if (to) {
+    return (
+      <StyledButton
+        size={size}
+        variant={variant}
+        onBlur={onBlur}
+        onClick={onClick}
+        onFocus={onFocus}
+        as={Link}
+        to={to}
+        className={className}
+        width={width}
+      >
+        {children}
+      </StyledButton>
+    );
+  }
+  return (
+    <StyledButton
+      size={size}
+      variant={variant}
+      type={type}
+      disabled={disabled}
+      onBlur={onBlur}
+      onClick={onClick}
+      onFocus={onFocus}
+      className={className}
+      width={width}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+Button.propTypes = {
+  variant: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  size: PropTypes.number.isRequired,
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func,
+  to: PropTypes.string,
+  width: PropTypes.string,
+};
+
+Button.defaultProps = {
+  width: "100%",
+};
+
+export default Button;
