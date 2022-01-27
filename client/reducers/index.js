42b8ac196cdf4eab67c772eadd0034569abae694
imparang <jeongjaeyn@gmail.com>
2022-01-27 21:49:29 +0900

    #15 [FEAT] next-redux-wrapper에서 HYDRATE 셋팅하기

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,13 +1,15 @@
 import { HYDRATE } from "next-redux-wrapper";
 import { combineReducers } from "redux";
 
+import terms from "./terms";
+
 const rootReducer = (state, action) => {
   switch (action.type) {
     case HYDRATE:
       console.log("HYDRATE", action);
       return action.payload;
     default: {
-      const combinedReducer = combineReducers({});
+      const combinedReducer = combineReducers({ terms });
       return combinedReducer(state, action);
     }
   }
