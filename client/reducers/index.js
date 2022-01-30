66fd9edda130e9379dbb24bc63f497a1ea6dbbd3
imparang <jeongjaeyn@gmail.com>
2022-01-31 00:43:30 +0900

    #31 [FEAT] Step1 리덕스 적용

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,6 +1,7 @@
 import { HYDRATE } from "next-redux-wrapper";
 import { combineReducers } from "redux";
 
+import auth from "./auth";
 import terms from "./terms";
 import step1 from "./step1";
 
@@ -10,7 +11,7 @@ const rootReducer = (state, action) => {
       console.log("HYDRATE", action);
       return action.payload;
     default: {
-      const combinedReducer = combineReducers({ terms, step1 });
+      const combinedReducer = combineReducers({ auth, terms, step1 });
       return combinedReducer(state, action);
     }
   }
