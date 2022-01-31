8d8689ea3e7effe1d3c344c55ee8148cdfba14ee
imparang <jeongjaeyn@gmail.com>
2022-01-31 13:22:32 +0900

    #33 [FIX] 요청 데이터 형태 수정

--- client/api/auth.js
+++ client/api/auth.js
@@ -2,9 +2,9 @@ import { request } from ".";
 
 export const auth = {
   getCertification(telNumber) {
-    return request("post", "api/sendSMS", { telNumber });
+    return request("post", "api/sendSMS", { userName: telNumber });
   },
   authorize(telNumber, password) {
-    return request("post", "api/login", { telNumber, password });
+    return request("post", "api/login", { userName: telNumber, password });
   },
 };
