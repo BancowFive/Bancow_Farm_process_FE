bf7b96e4b229d615aa22c160348763c41e6bd7c0
imparang <jeongjaeyn@gmail.com>
2022-01-31 13:02:50 +0900

    #33 [FEAT] 본인인증 api 비동기 로직 리듀서에 추가

--- client/reducers/auth.js
+++ client/reducers/auth.js
@@ -1,15 +1,76 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { auth } from "../api";
+
+export const getCertification = createAsyncThunk(
+  "auth/getCertification",
+  async ({ telNumber }, { rejectWithValue }) => {
+    try {
+      const result = await auth.getCertification(telNumber);
+      return result;
+    } catch (error) {
+      return rejectWithValue(err.response.data);
+    }
+  },
+);
+
+export const authorize = createAsyncThunk(
+  "auth/authorize",
+  async ({ telNumber, password }, { rejectWithValue }) => {
+    try {
+      const result = await auth.authorize(telNumber, password);
+      return result;
+    } catch (error) {
+      return rejectWithValue(err.response.data);
+    }
+  },
+);
 
 const initialState = {
   telNumber: "",
+  password: "",
+  certificationLoading: false,
+  certificationDone: false,
+  certificationError: null,
+  autorizationLoading: false,
+  autorizationDone: false,
+  autorizationError: null,
 };
 
 const authSlice = createSlice({
-  name: "terms",
+  name: "auth",
   initialState,
   reducers: {
     inputTelNumber: (state, action) => {
       state.telNumber = action.payload;
+    },
+  },
+  extraReducers: {
+    [getCertification.pending.type]: (state, action) => {
+      state.certificationLoading = true;
+      state.certificationDone = false;
+      state.certificationError = null;
+    },
+    [getCertification.fulfilled.type]: (state, action) => {
+      state.certificationLoading = false;
+      state.certificationDone = true;
+      state.password = action.payload;
+    },
+    [getCertification.rejected.type]: (state, action) => {
+      state.certificationLoading = false;
+      state.certificationError = action.payload;
+    },
+    [authorize.pending.type]: (state, action) => {
+      autorizationLoading = true;
+      autorizationDone = false;
+      autorizationError = null;
+    },
+    [authorize.fulfilled.type]: (state, action) => {
+      autorizationLoading = false;
+      autorizationDone = true;
+    },
+    [authorize.rejected.type]: (state, action) => {
+      autorizationLoading = false;
+      autorizationError = action.payload;
     },
   },
 });
