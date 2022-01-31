31ef047d29b75d7e6c8d01545a03dd8e869970ea
dozukwang <dozukwang@gmail.com>
2022-01-31 18:52:55 +0900

    #24 [FEAT]: 실사일요청 페이지 제작(API호출/리덕스 제외 완료)
    1. 실사일요청 페이지 기본적인 기능들과 컴포넌트 연결 완료
    
    API호출 연결, Store에 데이터 가져오기, path 이동 설정 등 추가기능 필요

--- client/pages/schedule/index.js
+++ client/pages/schedule/index.js
@@ -1,32 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Calendar } from "../../components/atoms/Calendar";
+import { ToastBar } from "../../components/atoms/Toast";
 import { SelectedDate } from "../../components/atoms/EmphasizedWord";
+import { Button } from "../../components/atoms/Button";
+import { Container, Toast } from "./style";
 
 const Schedule = () => {
-  const [selectedDate, setselectedDate] = useState({});
+  const [selectedDate, setselectedDate] = useState();
+  const [isDisabledDay, setIsDisabledDay] = useState(false);
   //useSelect로 가져올 실사예정일
   const isDisable = ["20220210", "20220216", "20220213"];
 
-  // const getSelectedDay = day =>
-  //   selectedDate({
-  //     year: day.format("YYYY"),
-  //     month: day.format("MM"),
-  //     day: day.format("DD"),
-  //   });
+  const getSelectedDay = day =>
+    setselectedDate({
+      full: day.format("YYYYMMDD"),
+      year: day.format("YYYY"),
+      month: day.format("MM"),
+      day: day.format("DD"),
+    });
 
-  // useEffect(() => {
-  //   console.log(
-  //     `선택한 날짜는 ${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일`,
-  //   );
-  // }, [selectedDate]);
+  const onDisabledAction = bool => {
+    setIsDisabledDay(bool);
+  };
 
   return (
-    <div>
+    <Container>
       {selectedDate ? (
-        <SelectedDate>{`${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일`}</SelectedDate>
-      ) : null}
-      <Calendar></Calendar>
-    </div>
+        <h2>
+          선택한 날짜는
+          <br />
+          <SelectedDate>{`${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일`}</SelectedDate>{" "}
+          이에요
+        </h2>
+      ) : (
+        <h2>
+          방문을 원하는 날짜를
+          <br />
+          선택해주세요
+        </h2>
+      )}
+      <Calendar
+        onClick={getSelectedDay}
+        disabledDate={isDisable}
+        coloredDayInfo="실사 요청 가능일"
+        onDisabledAction={onDisabledAction}
+      ></Calendar>
+      <Toast>
+        <ToastBar show={isDisabledDay} width={"196.5px"}>
+          해당 날짜는 선택할 수 없습니다
+        </ToastBar>
+      </Toast>
+      <Button
+        variant={selectedDate ? "primary" : "ghost"}
+        size={60}
+        fixed
+        block
+        disabled={selectedDate ? false : true}
+        to={"/"}
+      >
+        확인
+      </Button>
+    </Container>
   );
 };
 
