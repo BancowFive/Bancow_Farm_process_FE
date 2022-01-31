993a3b5294e3ef3ca95ba5b7b698474fd62ba456
dozukwang <dozukwang@gmail.com>
2022-01-31 18:49:57 +0900

    #24 [FEAT]: 달력 Controller 버튼출력 변경기능 추가

--- client/components/atoms/Calendar/CalendarController.js
+++ client/components/atoms/Calendar/CalendarController.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import left from "../../../public/chevron_left.svg";
-import right from "../../../public/chevron_right.svg";
+import leftGray from "../../../public/chevron_left_gray.svg";
+import leftBlack from "../../../public/chevron_left_black.svg";
+import rightGray from "../../../public/chevron_right_gray.svg";
+import rightBlack from "../../../public/chevron_right_black.svg";
 import { StyledCalendarController } from "./style";
 
 export const CalendarController = ({ children, today, month, setMonth }) => {
+  const [isgoBack, setIsGoBack] = useState(false);
+  const [isgoForward, setIsGoForward] = useState(true);
+
+  useEffect(() => {
+    if (month.format("YYYYMM") === today.format("YYYYMM")) {
+      setIsGoBack(false);
+    } else {
+      setIsGoBack(true);
+    }
+
+    const monthDiff = Math.ceil(month.diff(today, "month", true));
+
+    if (monthDiff === 3) {
+      setIsGoForward(false);
+    } else {
+      setIsGoForward(true);
+    }
+  }, [month]);
+
   //달력 이동
   const changeMonth = change => {
     switch (change) {
       case "add":
         const addedMonth = month.add(1, "month");
         const monthDiff = Math.ceil(addedMonth.diff(today, "month", true));
-        if (monthDiff < 4) {
+        if (monthDiff <= 3) {
           setMonth(month.add(1, "month"));
         }
         break;
@@ -32,7 +53,12 @@ export const CalendarController = ({ children, today, month, setMonth }) => {
           changeMonth("subtract");
         }}
       >
-        <Image src={left} alt="왼쪽" width={24} height={24} />
+        <Image
+          src={isgoBack ? leftBlack : leftGray}
+          alt="왼쪽"
+          width={24}
+          height={24}
+        />
       </button>
       <span>{children}</span>
       <button
@@ -40,7 +66,12 @@ export const CalendarController = ({ children, today, month, setMonth }) => {
           changeMonth("add");
         }}
       >
-        <Image src={right} alt="왼쪽" width={24} height={24} />
+        <Image
+          src={isgoForward ? rightBlack : rightGray}
+          alt="왼쪽"
+          width={24}
+          height={24}
+        />
       </button>
     </StyledCalendarController>
   );
