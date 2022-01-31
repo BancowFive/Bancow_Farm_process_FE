a97a2d12f45a3799f35e6f34b24c2450e20fbb90
imparang <jeongjaeyn@gmail.com>
2022-01-31 13:01:38 +0900

    #33 [FEAT] 공용 api 요청 함수 생성

---
+++ client/api/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const DOMAIN = "http://localhost:3000";
+
+export const request = (method, url, data) => {
+  return axios({
+    method,
+    url: DOMAIN + url,
+    data,
+  })
+    .then(result => result.data)
+    .catch(error => {
+      console.error(error);
+      throw error.response;
+    });
+};
+
+export const user = {
+  fetchData() {
+    return request("get", "api/farm/checkInfo/:id");
+  },
+};
+
+export { auth } from "./auth";
+export { terms } from "./terms";
