dc84ea5f32453c8af15a515757fa1c60dbc66952
dozukwang <dozukwang@gmail.com>
2022-02-05 21:50:41 +0900

    #63 [STYLE]: 제출완료 페이지 PC버전 스타일 작업

--- client/pages/done/step1.js
+++ client/pages/done/step1.js
@@ -1,4 +1,5 @@
 import { Container, ButtonInfo, ImgContainer } from "./style";
+import { Header, Footer, ProgressHeader } from "../../components";
 import Image from "next/image";
 import cowAndMePic from "../../public/cow_plus_me.svg";
 import { Button, AverageDate, ProgressStep } from "../../components/atoms";
@@ -7,21 +8,39 @@ const step1 = () => {
   return (
     <>
       <Container>
-        <ProgressStep lineStyle={"dashed"} activeStep={1} />
-        <h1>1차 신청이 완료되었어요</h1>
-        <h2>
-          평균 <AverageDate>2일 이내</AverageDate>로
-          <br />
-          뱅카우가 연락드릴게요
-        </h2>
-        <ImgContainer>
-          <Image src={cowAndMePic} alt="소와 나" />
-        </ImgContainer>
-        <ButtonInfo>확인을 누르시면 메인페이지로 이동합니다</ButtonInfo>
+        <div className="content">
+          <Header />
+          <ProgressHeader
+            ProgressType="step"
+            activeStep={1}
+            className="progressHeader"
+          />
+          <ProgressStep
+            className="mobileProgressStep"
+            lineStyle={"dashed"}
+            activeStep={1}
+            growLineBorder={"4px"}
+          />
+          <h1>1차 신청이 완료되었어요</h1>
+          <h2>
+            평균 <AverageDate>2일 이내</AverageDate>로
+            <br />
+            뱅카우가 연락드릴게요
+          </h2>
+          <ImgContainer>
+            <Image src={cowAndMePic} alt="소와 나" />
+          </ImgContainer>
+          <ButtonInfo>확인을 누르시면 메인페이지로 이동합니다</ButtonInfo>
+        </div>
+        <div className="aside">
+          <Button className="link" variant="primary" size={60} block to="/">
+            확인
+          </Button>
+          <div className="footer">
+            <Footer />
+          </div>
+        </div>
       </Container>
-      <Button variant="primary" size={60} block fixed to="/">
-        확인
-      </Button>
     </>
   );
 };
