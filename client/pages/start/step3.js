9301a8b425b397d03cdd36c9f4db834db13c164d
dozukwang <dozukwang@gmail.com>
2022-02-10 21:59:16 +0900

    [FIX]: start 페이지 순서 변경

--- client/pages/start/step3.js
+++ client/pages/start/step3.js
@@ -2,25 +2,25 @@ import React, { useCallback, useEffect } from "react";
 import { Button } from "../../components/atoms";
 import { Footer, ProgressHeader } from "../../components";
 import { Container, ImgContainer } from "./style";
-import { startStep2 } from "../../reducers/step2";
+import { startStep3 } from "../../reducers/step2";
 import Image from "next/image";
 import cowAndMePic from "../../public/cow_plus_me.svg";
 import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "next/router";
 
-const Step2 = () => {
+const Step3 = () => {
   const userId = useSelector(state => state.auth.id);
   const dispatch = useDispatch();
   const router = useRouter();
   const moveAllowed = useSelector(state => state.step2.startStatus);
 
   useEffect(() => {
-    if (moveAllowed === "fulfilled") router.replace(`/submit/${userId}`);
+    if (moveAllowed === "fulfilled") router.replace(`/schedule/${userId}`);
   }, [moveAllowed]);
 
   const movePage = useCallback(() => {
     dispatch(
-      startStep2({ PageNum: "10", inProgress: "STEP2_IN_PROGRESS", userId }),
+      startStep3({ PageNum: "13", inProgress: "STEP3_IN_PROGRESS", userId }),
     );
   }, []);
 
@@ -60,4 +60,4 @@ const Step2 = () => {
   );
 };
 
-export default Step2;
+export default Step3;
